0f08c8bf6c179cfd48150b5cc6a82d0ec8254670
Muhammad Wafa <wafax.4@gmail.com>
2020-06-11 02:45:55 +0700

    setup for upload to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
+import os.path
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
 
 setup(
     name='igmov',
-    version='1.0.0',
+    version='1.0.1',
     description='Video generator using Ngupuk templates',
+    long_description= read_file('README.md'),
+    long_description_content_type='text/markdown',
     license='MIT',
     packages=['igmov'],
     author='Ngupuk',
