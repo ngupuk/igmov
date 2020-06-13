fcf43e2ca48e71f97658e58cfc2aa92ec071cc3b
Muhammad Wafa <wafax.4@gmail.com>
2020-06-13 12:45:40 +0700

    test generate vieo

--- book/book4.py
+++ book/book4.py
@@ -2,7 +2,8 @@
 import numpy as np
 import moviepy.editor as mpy
 import cv2
-import igmov.analizer
+import igmov.analyzer as anl
+import igmov.draw as drw
 
 audioPath = '../sample/sample.ogg'
 thumbPath = '../sample/thumb.jpg'
@@ -16,22 +17,22 @@ sr = int(sound.shape[0]/audio.duration)
 img_raw = cv2.imread(thumbPath)
 img_raw = cv2.resize(img_raw, (400, 400))
 
-def interpolate(signal, count=100):
-  spectrum = signal
-  xlog = np.log(np.array(range(1, spectrum.shape[0]+1)))
-  xnew = np.linspace(np.min(xlog), np.max(xlog), count)
-  newspectrum = np.interp(xnew, xlog, spectrum)
-  return newspectrum
+# def interpolate(signal, count=100):
+#   spectrum = signal
+#   xlog = np.log(np.array(range(1, spectrum.shape[0]+1)))
+#   xnew = np.linspace(np.min(xlog), np.max(xlog), count)
+#   newspectrum = np.interp(xnew, xlog, spectrum)
+#   return newspectrum
 
-def get_sound(t, length=255):
-  end = sr * t
-  start = end - (sr/fps)
-  start = 0 if start<0 else start
-  L = sound[int(start):int(end), 0]
-  # ax.set_xscale('log')
-  hasil = np.fft.fft(L, length)
-  freqs = np.fft.fftfreq(length)
-  return abs(hasil[:length//10])
+# def get_sound(t, length=255):
+#   end = sr * t
+#   start = end - (sr/fps)
+#   start = 0 if start<0 else start
+#   L = sound[int(start):int(end), 0]
+#   # ax.set_xscale('log')
+#   hasil = np.fft.fft(L, length)
+#   freqs = np.fft.fftfreq(length)
+#   return abs(hasil[:length//10])
 
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(1)
@@ -43,16 +44,19 @@ def generator(t):
   # img = np.zeros((400, 400, 3), np.uint8)
   # spectrum = igmov.analizer.Octave(get_sound(t))
   ax.clear()
-  ax.set_ylim(-10, 30)
-  x = get_sound(t)
-  newX = interpolate(x[:int(x.shape[0]/2)], 50)
-  for i, l in enumerate(newX):
-    baseY = 350
-    baseX = 20
-    spacing = 7
-    tall = int(l) if int(l) < 190 else 190
-    xx, yy = line(baseY-tall, baseX + i*spacing, baseY+int(tall*.5), baseX+i*spacing)
-    img[xx, yy] = [255, 255, tall]
+  # ax.set_ylim(-10, 30)
+  # x = get_sound(t)
+  newX = anl.spectrum_sig(sound[:, 0], t, count=(400-40)//4)
+  # newX = interpolate(x[:int(x.shape[0]/2)], 50)
+  img = drw.spectrum(img, newX, 20, 300, spacing=4, factor=1.8)
+  img = drw.spectrum(img, newX, 20, 320, spacing=4, factor=.3, direction='bottom')
+  # for i, l in enumerate(newX):
+  #   baseY = 350
+  #   baseX = 20
+  #   spacing = 7
+  #   tall = int(l) if int(l) < 190 else 190
+  #   xx, yy = line(baseY-tall, baseX + i*spacing, baseY+int(tall*.5), baseX+i*spacing)
+  #   img[xx, yy] = [255, 255, tall]
   # ax.semilogx(x[4:])
   # plt.scatter(range(len(newX)), newX)
   # plt.scatter(range(len(newX)), -newX*.3)
@@ -72,6 +76,8 @@ def generator(t):
   # img = cv2.putText(img, str(norm), (50, 50), font, 1, (255, 54, 2))
   return img
 
+# generator(5)
+# plt.imshow(generator(10))
 video = mpy.VideoClip(
           generator, 
           duration=audio.duration,
