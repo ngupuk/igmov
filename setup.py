3e607a2a0278b27ff6efc994275c0f9fc9c7b9ad
Muhammad Wafa <wafax.4@gmail.com>
2020-06-18 20:45:27 +0700

    update setup for template library

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup
-import os.path
-def read_file(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        return file.read()
 
 setup(
     packages=['igmov', 'igmov.template']
