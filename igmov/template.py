3d6d2650b420a899159279004e06e4f16ebf69bd
Muhammad Wafa <wafax.4@gmail.com>
2020-06-18 15:56:45 +0700

    cleaning up

--- igmov/template.py
+++
@@ -1,9 +0,0 @@
-class Template1:
-  """
-    Ngupuk Template 1
-    :param audio: string, path to audio string
-  """
-  def __init__(self, audio):
-    import moviepy.editor as MP
-    self.__MP = MP
-    self.audio = self.__MP.AudioFileClip(audio)
